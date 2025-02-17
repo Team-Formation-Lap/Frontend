75de2267c1719e5da73099340113d95e7f430e8b
Shin-Sujin <tnwls5621@tukorea.ac.kr>
2025-02-17 11:49:37 +0900

    design : 헤더에 라우터 추가함

--- my-project/src/components/Header_login.tsx
+++ my-project/src/components/Header_login.tsx
@@ -1,11 +1,21 @@
+import { useNavigate } from "react-router-dom"; // 페이지 이동용
+
 const Header_login = () => {
+  const navigate = useNavigate(); // navigate 훅 사용
+  const handleLogoClick = () => {
+    console.log("메인페이지로 이동");
+    navigate("/"); // state에 memberId 전달
+  };
   return (
     <div>
       {/* 헤더 */}
       <header className="flex items-center justify-between px-8 py-4 bg-white shadow-md">
-        <h1 className="text-3xl font-bold font-museo bg-gradient-to-b from-[#AAA0FF] via-[#4C40B5] to-[#2A1E95] text-transparent bg-clip-text">
+        <button
+          className="text-3xl font-bold font-museo bg-gradient-to-b from-[#AAA0FF] via-[#4C40B5] to-[#2A1E95] text-transparent bg-clip-text"
+          onClick={() => handleLogoClick()} // 클릭 이벤트 핸들러
+        >
           IntelliView
-        </h1>
+        </button>
         <div className="flex space-x-4">
           <button className="px-4 py-2 text-gray-700 border rounded-md hover:bg-gray-100">
             로그아웃
--- my-project/src/components/reports/QuestionReport.tsx
+++ my-project/src/components/reports/QuestionReport.tsx
@@ -1,3 +1,9 @@
+import face1 from "../../assets/face1.png";
+import face2 from "../../assets/face2.png";
+import face3 from "../../assets/face3.png";
+import face4 from "../../assets/face4.png";
+import face5 from "../../assets/face5.png";
+
 const QuestionReport = () => {
   return <div>질문별 레포트 내용</div>;
 };
